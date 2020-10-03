05559e11c567568cf2821791dc22281df4b7245a
a.thiry <a.thiry@str8labs.com>
2020-10-03 23:48:04 +0200

    Some stuff

---
+++ Assets/Scripts/MainMenu/CameraRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MainMenu
+{
+    public class CameraRotation : MonoBehaviour
+    {
+        public float cameraSpeed;
+        public Transform target;
+        
+        void Update()
+        {
+            transform.RotateAround(target.position, Vector3.up, cameraSpeed * Time.deltaTime);
+        }
+    }
+}
